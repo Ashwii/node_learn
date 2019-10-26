687fa22bd2c2482474717406625ae433eff3d89d
acestra <acestranetorks.com>
2019-10-26 15:08:00 +0530

    Ash: Geo Location Weather

---
+++ geoWeather/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+const request = require('request');
+const apiKey = '85203ff3b0b8d49711ca43bf88d80015';
+app.post('/', function (req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    request(url, function (err, response, body) {
+        if(err){
+          res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weather = JSON.parse(body)
+          if(weather.main == undefined){
+            res.render('index', {weather: null, error: 'Error, please try again'});
+          } else {
+            let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+            res.render('index', {weather: weatherText, error: null});
+          }
+        }
+      });
+})
+app.use(express.static('public'));
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
